278277626ef377ade42abea902024abf55208c11
matthew.horsell <matthew.horsell@1d02a868-0ddd-11de-a490-4563d82dadc6>
2009-03-13 00:45:13 +0000

    reformated code and fixed systray not showing properly. thanks to kumyco(arch community)
    git-svn-id: http://batterymon.googlecode.com/svn/trunk@5 1d02a868-0ddd-11de-a490-4563d82dadc6

--- batterymon.py
+++ batterymon.py
@@ -22,151 +22,142 @@
 import gtk
 import gobject
 import sys, os
-VERSION="0.4.0"
+VERSION="0.5.0"
 
 #basepath="/home/matthew/Projects/batman/"
-basepath= os.path.join(sys.path[0], sys.argv[0])
+
+basepath = os.path.abspath(os.path.dirname(__file__))
+
 batpath="/sys/class/power_supply/"
 interval=30000
 #batpath="/home/matthew/Projects/systray/"
 class systray:
 
-	def alert( self, widget, data=None):
-		stat 		   = self.status()
-		perc 		   = self.percent()
-		perc1=int(perc)
-		dialog = gtk.MessageDialog(
-		parent         = None,
-		flags          = gtk.DIALOG_DESTROY_WITH_PARENT,
-		type           = gtk.MESSAGE_INFO,
-		buttons        = gtk.BUTTONS_OK,
-		message_format = "Battery Status "+stat +"\n" +"Current Charge " +str(perc1) +"%" +"\n"+"Battery level is low please plug in your mains adaptor")
-		dialog.set_title('Batterymon')
-		dialog.connect('response', self.show_hide)
-		dialog.show()
-		
-	def __init__(self):
-		self.test= gtk.StatusIcon()
-		self.test.set_from_file(basepath+"icons/battery_full.png")
-		self.test.set_blinking(False)
-		self.test.connect("activate", self.activate)
-		self.test.connect("popup_menu", self.popup)
-		self.test.set_visible(True)
-		gobject.timeout_add(interval,self.update)
-		gtk.main()
+    def alert( self, widget, data=None):
+        stat            = self.status()
+        perc            = self.percent()
+        dialog = gtk.MessageDialog(
+        parent         = None,
+        flags          = gtk.DIALOG_DESTROY_WITH_PARENT,
+        type           = gtk.MESSAGE_INFO,
+        buttons        = gtk.BUTTONS_OK,
+        message_format = "Battery Status %s\nCurrent Charge %d\
+          %%\nBattery level is low please plug in your mains adaptor"   \
+          % (stat, perc)
+        )
+        dialog.set_title('Batterymon')
+        dialog.connect('response', self.show_hide)
+        dialog.show()
+    
+    def set_icon(self, name):
+        print "%s/icons/battery_%s.png" % (basepath, name)
+        self.test.set_from_file("%s/icons/battery_%s.png" % (basepath, name))
+    
+    def __init__(self):
+        self.test= gtk.StatusIcon()
+        self.set_icon("full")
+        self.test.set_blinking(False)
+        self.test.connect("activate", self.activate)
+        self.test.connect("popup_menu", self.popup)
+        self.test.set_visible(True)
+        gobject.timeout_add(interval, self.update)
+        gtk.main()
 
-	def status(self):
-		FILE=open(batpath+"BAT0/status","r")
-		CMD=FILE.read()
-		FILE.close
-		return CMD
+    def status(self):
+        FILE=open(batpath+"BAT0/status","r")
+        CMD=FILE.read()
+        FILE.close
+        return CMD
 
-	def percent(self):
-		FILE=open(batpath+"BAT0/charge_full","r")
-		MAX=FILE.read()
-		FILE.close
-		FILE=open(batpath+"BAT0/charge_now","r")
-		NOW=FILE.read()
-		FILE.close
-		
-		per=(float(NOW) / float(MAX) * 100)
-		return (per)
+    def percent(self):
+        
+        FILE=open(batpath+"BAT0/charge_full","r")
+        MAX=FILE.read()
+        FILE.close
+        FILE=open(batpath+"BAT0/charge_now","r")
+        NOW=FILE.read()
+        FILE.close
+        
+        # only the int value is ever used
+        per = int(round(float(NOW) / float(MAX) * 100))
+        return (per)
 
-	def activate( self, widget, data=None):
-		stat 		   = self.status()
-		perc 		   = self.percent()
-		perc1=int(perc)
-		dialog = gtk.MessageDialog(
-		parent         = None,
-		flags          = gtk.DIALOG_DESTROY_WITH_PARENT,
-		type           = gtk.MESSAGE_INFO,
-		buttons        = gtk.BUTTONS_OK,
-		message_format = "Battery Status "+stat +"\n" +"Current Charge " +str(perc1) +"%")
-		dialog.set_title('Batterymon')
-		dialog.connect('response', self.show_hide)
-		dialog.show()
-	
-	def  show_hide(self, widget,response_id, data= None):
-		if response_id == gtk.RESPONSE_YES:
-			widget.hide()
-		else:
-			widget.hide()
-           
+    def activate( self, widget, data=None):
+        stat            = self.status()
+        perc            = self.percent()
+        perc1=perc
+        dialog = gtk.MessageDialog(
+        parent         = None,
+        flags          = gtk.DIALOG_DESTROY_WITH_PARENT,
+        type           = gtk.MESSAGE_INFO,
+        buttons        = gtk.BUTTONS_OK,
+        message_format = "Battery Status %s\nCurrent Charge %d%%" % (stat, perc))
+        dialog.set_title('Batterymon')
+        dialog.connect('response', self.show_hide)
+        dialog.show()
+    
+    def  show_hide(self, widget,response_id, data= None):
+        if response_id == gtk.RESPONSE_YES:
+            widget.hide()
+        else:
+            widget.hide()
+
 
     # destroyer callback
-	def  destroyer(self, widget,response_id, data= None):
-		if response_id == gtk.RESPONSE_OK:
-			gtk.main_quit()
-		else:
-			widget.hide()
-			
-	def popup(self, button, widget, data=None):
-		dialog = gtk.MessageDialog(
-		parent         = None,
-		flags          = gtk.DIALOG_DESTROY_WITH_PARENT,
-		type           = gtk.MESSAGE_INFO,
-		buttons        = gtk.BUTTONS_OK_CANCEL,
-		message_format = "Do you want to close me?")
-		dialog.set_title('Batterymon')
-		dialog.connect('response', self.destroyer)
-		dialog.show()
-	
-	def update(self):
-		CMD=self.status()
-		per=self.percent()
-		per1=int(per)
-		self.test.set_tooltip("Battery status "+ CMD + "Current charge " + str(per1) + "%")
-
-		if CMD.strip() =="Full":
-			self.test.set_from_file(basepath+"icons/battery_full.png")
-			gobject.timeout_add(interval,self.update)
-			return()
-
-		if CMD.strip()=="Charging":
-			self.test.set_from_file(basepath+"icons/battery_power.png")
-			gobject.timeout_add(interval,self.update)
-			return()
-
-		if CMD.strip()=="Discharging":
-				if round(per) <= 100.0:
-					self.test.set_from_file(basepath+"icons/battery_full.png")
-					self.test.set_blinking(False)	
-
-				if round(per) <= 87.0:
-					self.test.set_from_file(basepath+"icons/battery_7.png")
-					
-				if round(per) <= 75.0:
-					self.test.set_from_file(basepath+"icons/battery_6.png")
-					
-				if round(per) <= 62.0:
-					self.test.set_from_file(basepath+"icons/battery_5.png")
-					
-				if round(per) <= 50.0:
-					self.test.set_from_file(basepath+"icons/battery_4.png")
-					
-				if round(per) <= 37.0:
-					self.test.set_from_file(basepath+"icons/battery_3.png")
-					
-				if round(per) <= 25.0:
-					self.test.set_from_file(basepath+"icons/battery_2.png")
-				
-				if round(per) <= 12.0:
-					self.test.set_from_file(basepath+"icons/battery_1.png")
-
-				if round(per) <= 8.0:
-					self.test.set_from_file(basepath+"icons/battery_empty!.png")
-
-				if round(per) <= 5.0:
-					self.test.set_from_file(basepath+"icons/battery_empty.png")
-					self.test.set_blinking(True)
-					self.alert(self)
-				gobject.timeout_add(interval,self.update)
-	
-	
+    def  destroyer(self, widget,response_id, data= None):
+        if response_id == gtk.RESPONSE_OK:
+            gtk.main_quit()
+        else:
+            widget.hide()
+            
+    def popup(self, button, widget, data=None):
+        dialog = gtk.MessageDialog(
+        parent         = None,
+        flags          = gtk.DIALOG_DESTROY_WITH_PARENT,
+        type           = gtk.MESSAGE_INFO,
+        buttons        = gtk.BUTTONS_OK_CANCEL,
+        message_format = "Do you want to close me?")
+        dialog.set_title('Batterymon')
+        dialog.connect('response', self.destroyer)
+        dialog.show()
+    
+    def update(self):
+        cmd = self.status().strip()
+        per = int(round(self.percent()))
+        self.test.set_tooltip("Battery status: %s | Current charge: %d%%" % (cmd, per))
+        
+        if cmd == "Full":
+            self.set_icon("full")
+            self.set_icon("power")
+        elif cmd == "Discharging":
+            if per > 87:
+                self.set_icon("full")
+                self.test.set_blinking(False)
+            elif per > 75:
+                self.set_icon("7")
+            elif round(per) > 62:
+                self.set_icon("6")
+            elif per > 50:
+                self.set_icon("5")
+            elif per > 37:
+                self.set_icon("4")
+            elif per > 25:
+                self.set_icon("3")
+            elif per > 12:
+                self.set_icon("2")
+            elif per > 8:
+                self.set_icon("1")
+            elif per > 5:
+                self.set_icon("empty!")
+            else:
+                self.set_icon("empty")
+                self.test.set_blinking(True)
+                self.alert(self)
+        else:
+            pass
+            # should this cause an error?
+    
+    
 if __name__ == "__main__":
-	
-	tray=systray()
-
-
-	
-	
+    
+    tray=systray()
